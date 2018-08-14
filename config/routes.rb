436c1552fc1b3a135fb8615e93d627e7e2e1c185
romain sanson <romainsanson.gm@gmail.com>
2018-08-14 17:11:07 +0200

    wiki done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'articles/new', to: 'articles#new', as: :new
+  post '/articles', to: 'articles#create'
+
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  get '/articles/edit/:id', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
+
+  delete "articles/:id", to: 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+puts "Creating 10 fake posts"
+10.times do
+  article = Article.new(
+    title: Faker::DragonBall.character,
+    content: Faker::Lorem.paragraph(2)
+  )
+  article.save
+end
+puts "Finished"
